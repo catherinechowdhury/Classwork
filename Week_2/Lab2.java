7c4ce556b7d90e48e421de924b4c35bee5c2c97b
Catherine Chowdhury <luna2soriano@gmail.com>
2026-01-27 22:37:54 -0500

    Create Practice Problems

--- Week_2/Lab2.java
+++ Week_2/Lab2.java
@@ -3,9 +3,33 @@ import java.util.*;
 
 public class Lab2{
 
+    /**
+     * Fill Array
+     * 
+     * This method will fill the array with 10 random numbers
+     * 
+     * @param array
+     */
 
+    /**
+     * Above Average
+     * 
+     * This method will count the number of numbers
+     * above average
+     * 
+     * @param args
+     */
 
+    /**
+     * Print 
+     * 
+     * This method will print out the array
+     * @param args
+     */
+    
     public static void main(String[] args){
-        
+        //Create an integer array
+
+
     }
 }
